fe89c973b8334f8ca7448fa794928b0dbc9110f3
Panagiotis Favvatas <pfavvatas@yahoo.com>
2023-06-24 18:14:51 +0300

    write file and add some file names and extesnions

---
+++ utils/files.py
@@ -0,0 +1,23 @@
+import os
+import json
+from enum import Enum
+
+class FileExtensions(Enum):
+    JSON = '.json'
+    TXT = '.txt'
+    CSV = '.csv'
+    PDF = '.pdf'
+    PNG = '.png'
+    JPEG = '.jpeg'
+    MP4 = '.mp4'
+    MP3 = '.mp3'
+    DOCX = '.docx'
+
+class FileNames(Enum):
+    DATA = 'Data'
+    COMBINATIONS = 'Combinations'
+
+def writeToFile(folder_name, file_name, file_extension, data):
+    os.makedirs(folder_name, exist_ok=True)
+    with open(os.path.join(folder_name, ''.join([file_name, file_extension])), 'w') as f:
+        json.dump(data, f, indent=4)
