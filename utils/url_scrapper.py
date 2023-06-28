e8feca28d9af6e8671a0142f108a027d56439328
Panagiotis Favvatas <pfavvatas@yahoo.com>
2023-06-28 20:50:49 +0300

    search by id and create colors

--- utils/url_scrapper.py
+++ utils/url_scrapper.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import json
+import random
 
 class HTMLTag:
     def __init__(self, webelement, driver, depth=0, parent_id=None):
@@ -57,3 +59,42 @@ class HTMLTag:
             print(self.depth * indent + " " + attr, value)
         for child in self.children:
             child.print_tree(indent)
+
+    def find_tag_name_by_id(self, unique_id):
+        # Check if the current node is the one we're looking for
+        if str(self.unique_id) == str(unique_id):
+            return self.tag_name
+
+        # If it's not, look in each of the node's children
+        for child in self.children:
+            result = child.find_tag_name_by_id(unique_id)
+            if result is not None:
+                return result
+
+        # If the node wasn't found in this branch of the tree, return None
+        return None
+    
+    def random_color(self):
+        r = lambda: random.randint(0,255)
+        return '#%02X%02X%02X' % (r(),r(),r())
+
+    def generate_tag_color_map(self, combination_list, existing_map_file):
+        # Load the existing map if any
+        try:
+            with open(existing_map_file, 'r') as f:
+                color_map = json.load(f)
+        except FileNotFoundError:
+            color_map = {"tag_colors": {}}
+
+        for combination in combination_list:
+            # Convert ids to tag names
+            tag_names = [self.find_tag_name_by_id(id) for id in combination]
+            tag_key = ",".join(tag_names)
+
+            # Add to map if not already present
+            if tag_key not in color_map["tag_colors"]:
+                color_map["tag_colors"][tag_key] = self.random_color()
+
+        # Save the updated map
+        with open(existing_map_file, 'w') as f:
+            json.dump(color_map, f, indent=4)
