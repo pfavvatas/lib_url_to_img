c6d0dbe6460df98cb4ca1a16ad2404ed42257e08
Panagiotis Favvatas <pfavvatas@yahoo.com>
2023-06-28 20:51:43 +0300

    update init

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
 from .url_scrapper import HTMLTag
 from .config import Config
-from .combinations import generate_combinations_reversed, search_multiple_lengths, search_combinations_by_length
+from .combinations import generate_combinations_reversed, search_multiple_levels, search_combinations_by_level,get_combinations_tags_by_unique_id
 from .files import writeToFile, FileExtensions, FileNames
